6dbaf288a2f56a2c15082c83ef56801971e37e72
Heather Leyssenaar <48036336+lysna87@users.noreply.github.com>
2023-04-18 20:04:01 +0930

    Added rotation and position changes to new graphs
    Rotation isn't quite working correctly, will need to do some more trial and error

--- Assets/Swordfish/GraphCreator.cs
+++ Assets/Swordfish/GraphCreator.cs
@@ -7,21 +7,27 @@ using System.Linq;
 
 public class GraphCreator : GraphAxes
 {
+    //Graph prefab is based on the original Input data using different rocket types and weights.
     public GameObject GraphPrefab;
     public string inputFolderName = "Default_Inputs";
     private string inputFolderPath = "/Resources/AdditionalOutputs/";
 
     public string[] availableInputs;
+    public int graphsCreated = 0;
+    public float radius = 6;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         // Creates a list of variables from the given file
         variables = new List<string>();
         variables.AddRange(variableExtractionFile.text.Substring(0, variableExtractionFile.text.IndexOf(System.Environment.NewLine)).Split(','));
 
+        //find the folder names in the Additional Outputs Folder
         DirectoryInfo[] directories = new DirectoryInfo(Application.dataPath + inputFolderPath).GetDirectories();
 
+        //Get the different types of datasets where the input variable was changed (burnout, temperature).
         availableInputs = new string[directories.Length];
         for (int i = 0; i < directories.Length; i++)
         {
@@ -33,11 +39,15 @@ public class GraphCreator : GraphAxes
     // Creates a graph, based on the given prefab and set axes
     public void CreateGraph()
     {
+        
+        transform.RotateAround(transform.position, Vector3.up, graphsCreated*30);
+        graphsCreated++;
         // Creates graph and sets its axis variables
-        GameObject graph = Instantiate(GraphPrefab);
+        //transform.RotateAround(transform.position, 30);
+        GameObject graph = Instantiate(GraphPrefab,SpawnInCircle(transform.position, radius), transform.rotation);
         setGraphAxisVariables(graph);
 
-        //StartCoroutine(graph.GetComponentInChildren<DataFiles>().setSimulationFiles(inputFolderPath, inputFolderName));
+        //Change the folder here
         graph.GetComponentInChildren<DataFiles>().setSimulationPath(inputFolderPath, inputFolderName);
 
         // Sets the variables for the graph config. The config will still automatically get these variables after creations,
@@ -48,5 +58,17 @@ public class GraphCreator : GraphAxes
         graphConfig.xAxis = xAxis;
         graphConfig.yAxis = yAxis;
         graphConfig.zAxis = zAxis;
+
+    }
+
+    Vector3 SpawnInCircle(Vector3 center, float radius)
+    {
+        //float ang = Random.value * 360;
+        float ang = graphsCreated * 30;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
     }
 }
