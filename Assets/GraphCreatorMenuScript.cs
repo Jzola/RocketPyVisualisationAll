2c7205ebcdc40b18254df938165d222c372ca1c3
Heather Leyssenaar <48036336+lysna87@users.noreply.github.com>
2023-04-26 12:34:14 +0930

    Dropdown list input fixes
    There are now dropdowns for all 3 axis, and logic has been implemented for all 4 dropdowns, however the way I've done the axes (because it uses the same logic, I reused the listener) will definitely need testing.

--- Assets/GraphCreatorMenuScript.cs
+++ Assets/GraphCreatorMenuScript.cs
@@ -4,6 +4,7 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using IATK;
+using System;
 
 public class GraphCreatorMenuScript : MonoBehaviour
 {
@@ -11,64 +12,61 @@ public class GraphCreatorMenuScript : MonoBehaviour
     public GraphCreator gCreator;
     //this only exists on a graph at runtime after creation, so may need another way to access the generated data sets
     public GraphConfig gConfig;
-    public Dropdown axisDropdown;
+    public Dropdown xaxisDropdown;
+    public Dropdown yaxisDropdown;
+    public Dropdown zaxisDropdown;
     public Dropdown variableDropdown;
     public ToggleGroup graphTypeChoice;
     public ToggleGroup dimensionsChoice;
     public Text debugText;
     public Button createGraphButton;
-    public List<int> axesChosen = new List<int>();
+    public List<string> axesChosen = new List<string>();
+    public string xaxisChosen;
+    public string yaxisChosen;
+    public string zaxisChosen;
+    public string inputvariableChosen;
 
     // Start is called before the first frame update
     void Start()
     {
+        //GraphCreator has access to GraphCommon fields
+
         Canvas canvas = this.GetComponent<Canvas>();
+        List<string> variableList = gCreator.variables;
         //axisDropdown = canvas.GetComponentInChildrenWithTag //cannot get 'WithTag' to work. Understanding needed for Heather to use.
         //TODO in future sprint - get list from available input type folders (count the CSVs)
-        axisDropdown.options.Clear();
-        //Sprint 3, just use 1-30 by default
-        List<string> optionsList = new List<string>();
-        for (int i = 1; i < 31; i++)
-        {
-            optionsList.Add("" + i);
-        }
-        //populate the axes dropdown 
-        axisDropdown.AddOptions(optionsList);
+        xaxisDropdown.options.Clear();
+        yaxisDropdown.options.Clear();
+        zaxisDropdown.options.Clear();
 
-        //create listener
-        axisDropdown.onValueChanged.AddListener(delegate { axisDropdownItemSelected(axisDropdown); });
+        //Dropdown.OptionData xOptionData;
+        //xOptionData = xaxisDropdown.
+        //yaxisDropdown.options.Remove(xOptionData);
+
+        //foreach (string var in variableList)
+        //{
+        //    optionsList.Add(var);
+        //}
+        //populate the axes dropdown 
+        xaxisDropdown.AddOptions(variableList);
+        yaxisDropdown.AddOptions(variableList);
+        zaxisDropdown.AddOptions(variableList);
+
+        //create listeners. to be tested in VR.
+        xaxisDropdown.onValueChanged.AddListener(delegate { axisDropdownItemSelected(xaxisDropdown); });
+        yaxisDropdown.onValueChanged.AddListener(delegate { axisDropdownItemSelected(yaxisDropdown); });
+        zaxisDropdown.onValueChanged.AddListener(delegate { axisDropdownItemSelected(zaxisDropdown); });
 
 
         //populate the variable drop down menu
         variableDropdown.options.Clear();
         //we want the default_inputs chosen to start with.
+        variableDropdown.AddOptions(gCreator.availableInputs);
 
-        //TODO check folders that are present 
-        //get folders 
-        string debugTextStr = "";
-        string path = "Assets\\Resources\\AdditionalOutputs";
-        string[] folder_paths = Directory.GetDirectories(path);
+        variableDropdown.onValueChanged.AddListener(delegate { variableDropdownItemSelected(variableDropdown); });
 
-        List<string> variableOptionsList = new List<string>();
-        //folder_paths = Directory.
-        foreach (string folder in folder_paths)
-        {
-            //display without the Assets Resources path
-            //
-            string newfolder = folder.Replace(path+"\\", "");
-            //this folder has sub folders "Env1, Env2" etc. so is not set up correctly to be automatically loaded.
-            if(!newfolder.Equals("WeatherChangeMass"))
-                variableOptionsList.Add(newfolder);
-            Dropdown.OptionData odata = new Dropdown.OptionData();
-            odata.text = newfolder;
-            
-            debugTextStr = debugTextStr + newfolder +"\n";
-        }
-
-        variableDropdown.AddOptions(variableOptionsList);
-
-        debugText.text = debugTextStr;
-        //debugText.enabled = false; //if not using the debug text.
+        debugText.text = "";
+        debugText.enabled = false; //if not using the debug text.
         //Path.
 
 
@@ -76,20 +74,35 @@ public class GraphCreatorMenuScript : MonoBehaviour
 
 
     }
+
+    private void variableDropdownItemSelected(Dropdown variableDropdown)
+    {
+        int index = variableDropdown.value;
+        inputvariableChosen = variableDropdown.options[index].text;
+        
+    }
+
     [ContextMenu("Choose Axis")] //Heather doesn't know how to make this show up in inspector yet.
     private void axisDropdownItemSelected(Dropdown axisDropdown)
     {
         //TODO later sprint: Put the chosen axes into a list to be used for filtering the graph output
         //TODO find out if the Filtering UI will handle whether the .csv suffix is required
         int index = axisDropdown.value;
-        //do something with the index
-        int axis = int.Parse(axisDropdown.options[index].text);
-        //if the axis is not contained in the list then add it to the list for filtering (to be used in later sprint).
+        //do something with text
+        string axis = axisDropdown.options[index].text;
+        //for troubleshooting.
         if (!axesChosen.Contains(axis))
-            axesChosen.Add(int.Parse(axisDropdown.options[index].text));
+            axesChosen.Add(axisDropdown.options[index].text);
         else
             axesChosen.Remove(axis);
 
+        //TODO: test in VR if the correct axis chosen is filled with the user choice. Otherwise will need to create separate listeners. 
+        if (axisDropdown.Equals(xaxisDropdown))
+            xaxisChosen = axisDropdown.options[index].text;
+        else if (axisDropdown.Equals(yaxisDropdown))
+            yaxisChosen = axisDropdown.options[index].text;
+        else
+            zaxisChosen = axisDropdown.options[index].text;
         //TODO use some visual aid to show that axes are removed or re-added to the filter list (maybe a text box that refreshes the list
         //OR if can add a checkbox to the drop down options
 
@@ -101,7 +114,8 @@ public class GraphCreatorMenuScript : MonoBehaviour
     {
         //link to the graph creator
         //TODO get David's advice on whether the create graph can be set up this way.
-        //gCreator.variables.
+        //can also access Graph Common
+        //gCreator.
         //gCreator.dimensions = 
 
         //create the graph with chosen parameters.
