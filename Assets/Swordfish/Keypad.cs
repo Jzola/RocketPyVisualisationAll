b42d3691bc368bd0a29225574aa10e0d6e4c8eea
David G <david0o@internode.on.net>
2023-05-19 17:39:43 +0930

    Added new object transform linking script, fixed keypad bug
    A new script is now used for linking objects, LinkTransform.
    It will lock the transform of one object to the object the script is attached to.
    Fixed a bug with the keypad where it would through a null error when clicking a number before an input field.

--- Assets/Swordfish/Keypad.cs
+++ Assets/Swordfish/Keypad.cs
@@ -12,18 +12,21 @@ public class Keypad : MonoBehaviour
     // Add button value to the input field
     public void appendValue(string buttonNum)
     {
+        if (lastFocusedInputField == null) return;
         lastFocusedInputField.text += buttonNum;
     }
 
     // Backspace input field
     public void backspace()
     {
+        if (lastFocusedInputField == null) return;
         lastFocusedInputField.text = lastFocusedInputField.text.Substring(0, lastFocusedInputField.text.Length - 1);
     }
 
     // Get value from the input field as a float
     public float getValue()
     {
+        if (lastFocusedInputField == null) return 0;
         return float.Parse(lastFocusedInputField.text);
     }
 
