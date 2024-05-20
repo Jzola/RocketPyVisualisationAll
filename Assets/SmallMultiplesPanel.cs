57d136b07939a6979ddd86853c28fc3fa705e188
unknown <ECL@INM268661.UniNet.unisa.edu.au>
2024-05-20 16:12:42 +0930

    Add branch for build that will be used to run the study in semester 2

--- Assets/SmallMultiplesPanel.cs
+++ Assets/SmallMultiplesPanel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,22 +10,46 @@ public class SmallMultiplesPanel : MonoBehaviour
     private Button spawnGraphButton;
     [SerializeField]
     private GameObject graphPrefab;
+    [SerializeField]
+    private GameObject variableSelection;
+    [SerializeField]
+    private GameObject spawnArea;
+    [SerializeField]
+    private Button clearGraphButton;
 
-    private string xVar = "# Time (s)";
-    private string yVar = " Z (m)";
+    private string xVar;
+    private string yVar;
+    private List<GameObject> graphs = new List<GameObject>();
 
-    private Vector3 offset = new Vector3(1, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
         spawnGraphButton.onClick.AddListener(delegate { spawnGraph(); });
+        clearGraphButton.onClick.AddListener(delegate { clearGraphs(); });
     }
 
     private void spawnGraph()
     {
-        Vector3 position = gameObject.transform.position + offset;
-        GameObject smallMultipleGraph = Instantiate(graphPrefab, position, Quaternion.identity);
-        SetupChart chart = smallMultipleGraph.GetComponentsInChildren<SetupChart>()[0];
-        chart.LoadData(xVar, yVar);
+        VariableSelection outputVariables = variableSelection.GetComponentInChildren<VariableSelection>();
+        List<string> selectedVariables = outputVariables.GetSelection();
+        xVar = selectedVariables[0];
+        foreach (string variable in selectedVariables.Skip(1))
+        {
+            yVar = variable;
+            GameObject smallMultipleGraph = Instantiate(graphPrefab, spawnArea.transform.position, Quaternion.identity, spawnArea.transform);
+            graphs.Add(smallMultipleGraph);
+            SetupChart chart = smallMultipleGraph.GetComponentInChildren<SetupChart>();
+            chart.LoadData(xVar, yVar);
+        }
+
+        outputVariables.ClearSelection();
+    }
+
+    private void clearGraphs()
+    {
+        foreach (GameObject graph in graphs)
+        {
+            graph.Destroy();
+        }
     }
 }
